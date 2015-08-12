393cc4dff23ebbfd7673bbbcf009cd0a947fafc8
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-12 22:17:18 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+	"unicode"
+)
+
+var tlds = []string{"com", "net", "io"}
+
+const allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789_-"
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		// converting text to lower case (obviously)
+		text := strings.ToLower(s.Text())
+		var newText []rune
+		for _, r := range text {
+			if unicode.IsSpace(r) {
+				r = '-'
+			}
+			if !strings.ContainsRune(allowedChars, r) {
+				continue
+			}
+			newText = append(newText, r)
+		}
+		// converting []rune slice to a string and outputing
+		fmt.Println(string(newText) + "." + tlds[rand.Intn(len(tlds))])
+	}
+}
